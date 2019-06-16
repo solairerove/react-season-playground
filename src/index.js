c34ed86b78aa51b2ab4eadf0618fcf6a896ed63f
Mikita Kryvitski <solairerove@gmail.com>
2019-06-16 21:41:02 +0300

    feature: use component did mount to load data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
@@ -11,29 +10,25 @@ class App extends React.Component {
             lat: null,
             errorMessage: ''
         };
+    }
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                console.log(position);
-                this.setState({ lat: position.coords.latitude })
-            },
-            err => {
-                console.log(err);
-                this.setState({ errorMessage: err.message });
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
     }
 
     render() {
-        return (
-            <div>
-                <div>Hello there !</div>
-                <br/>
-                <div>Latitude: {this.state.lat}</div>
-                <br/>
-                <div>Error: {this.state.errorMessage}</div>
-            </div>
-        );
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+
+        return <div>Loading</div>
     }
 }
 
