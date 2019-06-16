22d8f087a7fa3ab9a049db38f6c865b46957f89e
Mikita Kryvitski <solairerove@gmail.com>
2019-06-16 22:07:23 +0300

    feature: display current season situation

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+    console.log(lat, month);
+
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+
+    console.log(season);
+
+    const text = season === 'winter' ? 'Brah, it is chilly' : 'Lets hit the beach';
+
     return (
-        <div>Season</div>
+        <div>
+            <h1>{text}</h1>
+        </div>
     );
 };
 
